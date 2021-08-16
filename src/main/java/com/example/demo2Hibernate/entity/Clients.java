f08f47e28b13bea3063c7823b84cccda5044aa95
sdsAtom <sdshere@ya.ru>
2021-08-16 16:49:35 +0300

    1

--- src/main/java/com/example/demo2Hibernate/entity/Clients.java
+++ src/main/java/com/example/demo2Hibernate/entity/Clients.java
@@ -1,10 +1,15 @@
 package com.example.demo2Hibernate.entity;
 
 import javax.persistence.*;
+import javax.ws.rs.PathParam;
+import java.util.*;
 
 @Entity
-@NamedQuery(name = "Client.byID", query = "select a from Clients a where a.id >= ?1")
+@NamedQuery(name = "Client.byID", query = "select a from Clients a where a.id = ?1")
+@NamedQuery(name = "Client.full", query = "select a from Clients a")
 public class Clients {
+
+
     @Override
     public String toString() {
         return "Clients{" +
@@ -68,19 +73,138 @@ public class Clients {
         Clients clients = (Clients) o;
 
         if (id != clients.id) return false;
-        if (name != null ? !name.equals(clients.name) : clients.name != null) return false;
-        if (email != null ? !email.equals(clients.email) : clients.email != null) return false;
-        if (phone != null ? !phone.equals(clients.phone) : clients.phone != null) return false;
-
-        return true;
+        if (!Objects.equals(name, clients.name)) return false;
+        if (!Objects.equals(email, clients.email)) return false;
+        return Objects.equals(phone, clients.phone);
     }
 
     @Override
     public int hashCode() {
+
         int result = id;
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (email != null ? email.hashCode() : 0);
         result = 31 * result + (phone != null ? phone.hashCode() : 0);
         return result;
+    }
+
+    public void save() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sds_data_source");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        if  (this.id == 0) {
+            try {
+                transaction.begin();
+
+                Query countClients = entityManager.createNativeQuery("select max(a.id)+1 as cnt from clients  a");
+                this.id = (int) countClients.getSingleResult();
+
+                entityManager.merge(this);
+
+                transaction.commit();
+            } finally {
+
+                if (transaction.isActive()) {
+                    transaction.rollback();
+                }
+
+                entityManager.close();
+                entityManagerFactory.close();
+            }
+        } else {
+
+            try {
+                transaction.begin();
+
+                //entityManager.persist(this);
+                entityManager.merge(this);
+
+                transaction.commit();
+            } finally {
+
+                if (transaction.isActive()) {
+                    transaction.rollback();
+                }
+
+                entityManager.close();
+                entityManagerFactory.close();
+            }
+        }
+    }
+
+    public static Clients getClient(int id) {
+
+        Clients res = null;
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sds_data_source");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+
+            TypedQuery<Clients> empByIdQuery = entityManager.createNamedQuery("Client.byID", Clients.class);
+            empByIdQuery.setParameter(1, id);
+
+            //List<Clients> clients = empByIdQuery.getResultList();
+
+
+            for (Clients client : empByIdQuery.getResultList()) {
+                res = client;
+            }
+
+            transaction.commit();
+        } finally {
+
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+
+        return  res;
+    }
+
+    public static List<Clients> getClients(int id) {
+
+        List<Clients> res = null;
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sds_data_source");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+
+            if (id <= 0) {
+                TypedQuery<Clients> empByIdQuery = entityManager.createNamedQuery("Client.full", Clients.class);
+                //empByIdQuery.setParameter(1, id);
+                res = empByIdQuery.getResultList();
+            }
+            else {
+                TypedQuery<Clients> empByIdQuery = entityManager.createNamedQuery("Client.byID", Clients.class);
+                empByIdQuery.setParameter(1, id);
+
+                for (Clients client : empByIdQuery.getResultList()) {
+                    res = Collections.singletonList(client);
+                }
+            }
+
+
+            transaction.commit();
+        } finally {
+
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+
+        return  res;
     }
 }
--- src/main/java/com/example/demo2Hibernate/service/ClientSevice.java
+++ src/main/java/com/example/demo2Hibernate/service/ClientSevice.java
@@ -2,26 +2,27 @@ package com.example.demo2Hibernate.service;
 
 import com.example.demo2Hibernate.entity.Clients;
 
+import javax.persistence.*;
 import javax.ws.rs.*;
+import java.util.List;
 
 @Path("/client")
 public class ClientSevice {
 
+
     @GET
     @Path("/{id}")
     @Produces("application/json")
-    public Clients getClient(@PathParam("id") int id)
+    public List<Clients> getClient(@PathParam("id") int id)
     {
-        Clients client = new Clients();
-        client.setName("MyDesignStudio");
-        client.setId(id);
-
-        return client;
+        return Clients.getClients(id);
     }
 
     @POST
     @Consumes("application/json")
     public String setClient(Clients client) {
+
+        client.save();
 
         return client.toString();
     }
